d2e866db01aad7ff000d9584638545b2661b435a
wisdomanthoni <wizitendo10@gmail.com>
2019-08-07 18:15:55 +0100

    trait to interact with charge response

--- src/BushaPay.php
+++ src/BushaPay.php
@@ -3,10 +3,11 @@
 namespace Busha\BushaPay;
 
 use Busha\Concerns\HandleRequest;
+use Busha\Concerns\InteractsWithResponse;
 
 class BushaPay
 {
-    use HandleRequest;
+    use HandleRequest, InteractsWithResponse;
 
     public function __construct()
     {
@@ -19,25 +20,30 @@ class BushaPay
     public function showCharge($charge)
     {
         $this->get('/charges/'.$charge);
+        return $this;
     }
 
     public function listCharge($page,$limit)
     {
         $this->get('/charges', compact($page,$limit));
+        return $this;
     }
 
     public function createCharge($data)
     {
         $this->post('/charges', $data);
+        return $this;
     }
 
     public function cancelCharge($charge)
     {
         $this.post('/charges/'. $charge .'/cancel');
+        return $this;
     }
 
     public function resolveCharge($charge)
     {
         $this.post('/charges/'. $charge .'/resolve');
+        return $this;
     }
 }
---
+++ src/Concerns/InteractsWithResponse.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Busha\Concerns;
+
+trait InteractsWithResponse
+{
+    /**
+     * Get the charge addresses
+     * @return array
+     */
+    public function getAddresses()
+    {
+        return $this->getResponse()['data']['addresses'];
+    }
+
+    /**
+     * Get charge pricing
+     * @return array
+     */
+    public function getPricing()
+    {
+        return $this->getResponse()['data']['pricing'];
+    }
+
+    /**
+     * Get the charge access code from response
+     * @return string
+     */
+    public function getAccessCode()
+    {
+        return $this->getResponse()['data']['code'];
+    }
+
+    /**
+     * Get the charge id from the response
+     * @return string
+     */
+    public function getChargeId()
+    {
+        return $this->getResponse()['data']['id'];
+    }
+
+    /**
+     * Get the charge timeline from the response
+     * @return array
+     */
+    public function getTimeline()
+    {
+       return $this->getResponse()['data']['timeline'];
+    }
+
+    /**
+     * Get the metadata from the response
+     * @return array
+     */
+    public function getMetadata()
+    {
+       return $this->getResponse()['data']['metadata'];
+    }
+
+    /**
+     * Get the authorization url from the callback response
+     * @return string
+     */
+    public function getChargeUrl()
+    {
+        return $this->getResponse()['data']['hosted_url'];
+    }
+
+    /**
+     * Fluent method to redirect to Paystack Payment Page
+     */
+    public function redirectNow()
+    {
+        return redirect($this->getResponse()['data']['hosted_url']);
+    }
+
+    /**
+     * Get the whole response from a get operation
+     * @return array
+     */
+    private function getResponse()
+    {
+        return json_decode($this->response->getBody(), true);
+    }
+
+   /**
+     * Get the data response from a get operation
+     * @return array
+     */
+    private function getData()
+    {
+        return $this->getResponse()['data'];
+    }
+
+}
