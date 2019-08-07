fb0209b37844f682a6a6ca69bbb5f7493b6b2154
wisdomanthoni <wizitendo10@gmail.com>
2019-08-07 14:23:04 +0100

    update bushapay service provider

--- src/BushaPay.php
+++ src/BushaPay.php
@@ -2,7 +2,25 @@
 
 namespace Busha\BushaPay;
 
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Config;
+use Unicodeveloper\Paystack\Exceptions\IsNullException;
+use Unicodeveloper\Paystack\Exceptions\PaymentVerificationFailedException;
+
 class BushaPay
 {
-    // Build your next great package.
+    /**
+     * Get Base Url from Paystack config file
+     */
+    public function setBaseUrl()
+    {
+        $this->baseUrl = Config::get('bushapay.paymentUrl');
+    }
+    /**
+     * Get secret key from BushaPay config file
+     */
+    public function setKey()
+    {
+        $this->secretKey = Config::get('bushapay.secretKey');
+    }
 }
--- src/BushaPayServiceProvider.php
+++ src/BushaPayServiceProvider.php
@@ -14,30 +14,14 @@ class BushaPayServiceProvider extends ServiceProvider
         /*
          * Optional methods to load your package assets
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'busha-pay');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'busha-pay');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('busha-pay.php'),
+                __DIR__.'/../config/config.php' => config_path('bushapay.php'),
             ], 'config');
 
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/busha-pay'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/busha-pay'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/busha-pay'),
-            ], 'lang');*/
 
             // Registering package commands.
             // $this->commands([]);
@@ -50,7 +34,7 @@ class BushaPayServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'busha-pay');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'bushapay');
 
         // Register the main class to use with the facade
         $this->app->singleton('busha-pay', function () {
