845c15597f03c8232ebe4354d2399a3e66874929
wisdomanthoni <wizitendo10@gmail.com>
2019-08-07 17:35:33 +0100

    add busha pay config keys

--- config/config.php
+++ config/config.php
@@ -1,8 +1,21 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+    /**
+     * Api Key From Busha Pay Dashboard
+     *
+     */
+    'apiKey' => env('BUSHAPAY_API_KEY'),
 
+    /**
+     * Api Version For Busha Pay
+     *
+     */
+    'apiVersion' => env('BUSHAPAY_API_Version', "2019-06-30"),
+
+    /**
+     * Api Url For Busha Pay Service
+     *
+     */
+    'apiUrl' => env('BUSHAPAY_API_URL', 'api.pay.busha.co'),
 ];
